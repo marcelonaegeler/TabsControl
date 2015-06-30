4ccf036cb5fef61e4e40af986c625c8166323087
Marcelo <marcelo.andre.naegeler@gmail.com>
2015-06-29 21:38:55 -0300

    Added external JS and CSS, tested nested tab groups

---
+++ script.js
@@ -0,0 +1,63 @@
+(function() {
+	var tabPlugin = document.querySelectorAll('.tabGroup');
+
+	if(!tabPlugin)
+		return false;
+	
+	function TabElement(tabGroup) {
+		this.tabGroup = tabGroup;
+		this.tabItems = this.tabGroup.querySelector('.tabBody').children;
+		this.tabLinks = this.tabGroup.querySelector('.tabNav').children;
+
+		this.active = 0;
+	}
+
+	TabElement.prototype.setEvents = function() {
+		var tabLinks = this.tabLinks;
+
+		for(var i in tabLinks) {
+			if(!parseInt(i) && parseInt(i) != 0)
+				continue;
+
+			tabLinks[i].setAttribute('rel', i);
+			tabLinks[i].addEventListener('click', function(event) {
+				event.preventDefault();
+				this.active = parseInt(event.target.rel);
+				this.exec();
+			}.bind(this), false);
+		}
+	};
+
+	TabElement.prototype.exec = function() {
+		var tabItems = this.tabItems;
+		var tabLinks = this.tabLinks;
+
+		for(var i in tabItems) {
+			if(!parseInt(i) && parseInt(i) != 0)
+				continue;
+
+			parseInt(i) == this.active ? tabItems[i].style.display = "block" : tabItems[i].style.display = "none";
+		}
+
+		for(var i in tabLinks) {
+			if(!parseInt(i) && parseInt(i) != 0)
+				continue;
+			
+			parseInt(i) == this.active ? tabLinks[i].classList.add('active') : tabLinks[i].classList.remove('active');
+		}
+	};
+
+	TabElement.prototype.init = function() {
+		this.exec();
+		this.setEvents();
+	};
+
+	for(var group in tabPlugin) {
+		if(!parseInt(group) && parseInt(group) != 0)
+			continue;
+		var setEvents = true
+			, active = 0;
+
+		var tab = new TabElement(tabPlugin[group]).init();
+	}
+})();
